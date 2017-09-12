12e26050a41a89948f7ff3714f6b429a67af898a
Clive Alvares <clivealvares@xeadmins-imac.xe.com>
2017-09-12 17:05:54 -0400

    added basic views

---
+++ CurrencyChart/CurrencyChart/RoundButton.swift
@@ -0,0 +1,24 @@
+//
+//  RoundButton.swift
+//  CurrencyChart
+//
+//  Created by Clive Alvares on 2017-09-12.
+//  Copyright © 2017 Clive Alvares. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+    
+    
+    @IBInspectable var cornerRadius: CGFloat = 0
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ CurrencyChart/CurrencyChart/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CurrencyChart
+//
+//  Created by Clive Alvares on 2017-09-12.
+//  Copyright © 2017 Clive Alvares. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
