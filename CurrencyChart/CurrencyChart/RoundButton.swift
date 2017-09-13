f6dbe6526ceb696769001c5ac55e082b858bb240
Clive Alvares <clive.alvares@gmail.com>
2017-09-13 08:59:43 -0400

    made cells

--- CurrencyChart/CurrencyChart/RoundButton.swift
+++ CurrencyChart/CurrencyChart/RoundButton.swift
@@ -12,7 +12,26 @@ import UIKit
 class RoundButton: UIButton {
     
     
-    @IBInspectable var cornerRadius: CGFloat = 0
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        
+        didSet{
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        
+        didSet {
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = UIColor.clear {
+        
+        didSet {
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
