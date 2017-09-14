640d9d8513151e65fe1e829d3286bd7db01ead12
Clive Alvares <clivealvares@xeadmins-imac.xe.com>
2017-09-14 16:39:03 -0400

    constraints added

--- CurrencyChart/CurrencyChart/ViewController.swift
+++ CurrencyChart/CurrencyChart/ViewController.swift
@@ -13,6 +13,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+//        let dummyView = UIView()
+//        dummyView.translatesAutoresizingMaskIntoConstraints = false
+//        dummyView.backgroundColor = UIColor.red
+//        self.view.addSubview(dummyView)
+//        
+//        dummyView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0).isActive = true
+//        dummyView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 0).isActive = true
+//        dummyView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.5).isActive = true
+//        dummyView.heightAnchor.constraint(equalTo: dummyView.widthAnchor, multiplier: 1.0).isActive = true
     }
 
     override func didReceiveMemoryWarning() {
