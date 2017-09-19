f7026acc27e3cb34072e3ef5988ad8cbc96a36fb
Clive Alvares <calvares@edu.uwaterloo.ca>
2017-09-19 16:08:35 -0400

    creating objective C version

---
+++ CurrencyChartObjectiveC/CurrencyChartObjectiveC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CurrencyChartObjectiveC
+//
+//  Created by Clive Alvares on 2017-09-19.
+//  Copyright © 2017 Clive Alvares. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CurrencyChartObjectiveC/CurrencyChartObjectiveC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CurrencyChartObjectiveC
+//
+//  Created by Clive Alvares on 2017-09-19.
+//  Copyright © 2017 Clive Alvares. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
