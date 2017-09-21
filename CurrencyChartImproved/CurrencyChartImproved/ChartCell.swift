78f0f450ddb88320a3dcae711092405ff1d2e608
Clive Alvares <calvares@edu.uwaterloo.ca>
2017-09-21 15:52:35 -0400

    learning how image views work

--- CurrencyChartImproved/CurrencyChartImproved/ChartCell.swift
+++ CurrencyChartImproved/CurrencyChartImproved/ChartCell.swift
@@ -28,25 +28,63 @@ class ChartCell: UIView {
         view1.translatesAutoresizingMaskIntoConstraints = false
         view2 = UILabel(frame: CGRect.zero)
         view2.translatesAutoresizingMaskIntoConstraints = false
-        super.init(frame: CGRect(x:0,y:0, width:80, height:37))
+        let curLabel = view2 as! UILabel
+        curLabel.text = Currency
+        super.init(frame: CGRect.zero)
         
         self.translatesAutoresizingMaskIntoConstraints = false
-        
         if(Currency == "USD") {
             view1 = UIImageView(image: #imageLiteral(resourceName: "american-flag-small"))
-            self.addSubview(view1)
+            
         } else if(Currency == "AMD") {
             view1 = UIImageView(image: #imageLiteral(resourceName: "armenian-flag-small"))
-            self.addSubview(view1)
+            
         }
         
         view1.contentMode = .scaleAspectFit
+        view1.clipsToBounds = true
+        self.addSubview(view2)
+        self.addSubview(view1)
+        
+        view1.widthAnchor.constraint(equalTo: self.widthAnchor, constant:0).isActive = true
+        view2.widthAnchor.constraint(equalTo: view1.widthAnchor, constant:0).isActive = true
+        view2.heightAnchor.constraint(equalTo: view1.heightAnchor, constant:0).isActive = true
+        view1.centerXAnchor.constraint(equalTo: self.centerXAnchor, constant:0).isActive = true
+        view2.centerXAnchor.constraint(equalTo: self.centerXAnchor, constant:0).isActive = true
+        view2.topAnchor.constraint(equalTo:view1.bottomAnchor, constant:15).isActive = true
+        view1.topAnchor.constraint(equalTo: self.topAnchor, constant:0).isActive = true
+        view2.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant:0).isActive = true
+        view1.heightAnchor.constraint(equalTo: view2.heightAnchor, multiplier: 1.0).isActive = true
+//        
+    
+    }
+    
+    init(){
+        view1 = UILabel(frame: CGRect.zero)
+        view2 = UILabel(frame: CGRect.zero)
+        view1.translatesAutoresizingMaskIntoConstraints = false
+        view2.translatesAutoresizingMaskIntoConstraints = false
+        super.init(frame: CGRect.zero)
+        
+        self.translatesAutoresizingMaskIntoConstraints = false
+        let inverse = view2 as! UILabel
+        let rate = view1 as! UILabel
+        rate.text = "1.00000"
+        inverse.text = "1.00000"
+        self.addSubview(view1)
         self.addSubview(view2)
         
-        view1.leadingAnchor.constraint(equalTo: 0, constant: <#T##CGFloat#>)
+        view1.widthAnchor.constraint(equalTo: self.widthAnchor, constant:0).isActive = true
+        view2.widthAnchor.constraint(equalTo: view1.widthAnchor, multiplier: 1.0).isActive = true
+        view2.heightAnchor.constraint(equalTo: view1.heightAnchor, constant:0).isActive = true
+        view1.centerXAnchor.constraint(equalTo: self.centerXAnchor, constant:0).isActive = true
+        view2.centerXAnchor.constraint(equalTo: self.centerXAnchor, constant:0).isActive = true
+        view2.topAnchor.constraint(equalTo:view1.bottomAnchor, constant:0).isActive = true
+        view1.topAnchor.constraint(equalTo: self.topAnchor, constant:0).isActive = true
+        view2.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant:0).isActive = true
+
         
         
-    
     }
     
     required init?(coder aDecoder: NSCoder) {
--- CurrencyChartImproved/CurrencyChartImproved/ViewController.swift
+++ CurrencyChartImproved/CurrencyChartImproved/ViewController.swift
@@ -14,7 +14,32 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        let flagRow = rowMake(cells: [ChartCell(Currency: "USD")])
+        let labelRow0 = rowMake(cells: [ChartCell(), ChartCell(), ChartCell()])
+        let labelRow1 = rowMake(cells: [ChartCell(), ChartCell(), ChartCell()])
+        let labelRow2 = rowMake(cells: [ChartCell(), ChartCell(), ChartCell()])
         
+        let rowStack = UIStackView(arrangedSubviews: [flagRow, labelRow0, labelRow1, labelRow2])
+        rowStack.axis = .vertical
+        rowStack.distribution = .fillEqually
+        rowStack.alignment = .fill
+        rowStack.spacing = 2
+        rowStack.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(rowStack)
+        
+        rowStack.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.9).isActive = true
+        rowStack.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.5).isActive = true
+        rowStack.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        rowStack.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+    }
+    
+    func rowMake(cells: [ChartCell]) -> UIStackView {
+        let row = UIStackView(arrangedSubviews: cells)
+        row.axis = .horizontal
+        row.distribution = .fillEqually
+        row.alignment = .fill
+        row.translatesAutoresizingMaskIntoConstraints = false
+        return row
     }
 
     override func didReceiveMemoryWarning() {
